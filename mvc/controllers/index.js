d5cf554d304498173988f4197c614bdf8dd26109
Dyrits <dyrits@outlook.com>
2021-09-03 15:48:51 +0200

    Filter by tag added.

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -2,21 +2,24 @@ let { data, tags, categories } = require("../routes/data");
 const recent = data.slice(0, 5);
 
 const getHomePage = function(req, res) {
-    res.render("index", { title: "Just Me", posts: data, active: "index", categories: categories })
+    let options = { title: "Just Me", posts: data, active: "index", categories };
+    res.render("index", options);
 }
 
 const getBlogPost = function({ params }, res) {
     let post = data.find(post => post.id === Number(params.id));
-    post ? res.render("post", { title: post.title, post: post, tags: tags, recent: recent, categories: categories }) : res.redirect("/404");
+    post ? res.render("post", { title: post.title, post, tags, recent, categories }) : res.redirect("/404");
 }
 
 const getFilteredList = function({ query }, res) {
-    let posts = data.filter(post => post.category === query.category);
-    res.render("filter", { title: "Just Me - Filtered", active: query.category, posts: posts, categories: categories })
+    let posts = data.filter(post => post.category === query.category || post.tags.includes(query.tag));
+    let options = { title: "Just Me - Filtered", active: query.category, posts, categories };
+    res.render("filter", options);
 }
 
 const get404 = function(req, res) {
-    res.render("404", { title: "Not Found", tags: tags, recent: recent, categories: categories })
+    let options = { title: "Not Found", tags, recent, categories };
+    res.render("404", options);
 }
 
 const redirect404 = function(req, res) {
@@ -24,11 +27,13 @@ const redirect404 = function(req, res) {
 }
 
 const getAbout = function(req, res) {
-    res.render("about", { title: "About", active: "about", categories: categories });
+    let options = { title: "About", active: "about", categories }
+    res.render("about", options);
 }
 
 const getContact = function(req, res) {
-    res.render("contact", { title: "Contact", active: "contact", categories: categories });
+    let options = { title: "Contact", active: "contact", categories }
+    res.render("contact", options);
 }
 
 module.exports = { getHomePage, getBlogPost, getFilteredList, get404, redirect404, getAbout, getContact };
